0d0994f325c0ea961027a45122d4bcef13b37788
Adrien Bounader <56256255+adrbounaderwiilog@users.noreply.github.com>
2023-05-10 16:03:52 +0200

    WIIS-9705: 6.31.4#rfid with right sqlite plugin config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,11 +5,20 @@ const config: CapacitorConfig = {
     appName: 'Follow GT',
     webDir: 'www',
     bundledWebRuntime: false,
+    loggingBehavior: 'none',
     plugins: {
         PushNotifications: {
             presentationOptions: ["badge", "sound", "alert"],
         },
         LocalNotifications: {
+        },
+        CapacitorSQLite: {
+            androidIsEncryption: false,
+            androidBiometric: {
+                biometricAuth: false,
+                biometricTitle: "Biometric login for capacitor sqlite",
+                biometricSubTitle: "Log in using your biometric"
+            }
         }
     },
 };
