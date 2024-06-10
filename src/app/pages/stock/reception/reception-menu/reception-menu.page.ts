3258f5860de6f1e1eedb84cfc8a86410dfaef860
GregoireDolidon <gregoire.dolidon@wiilog.fr>
2024-06-10 14:18:00 +0200

    WIIS-11366

--- src/app/pages/stock/reception/reception-menu/reception-menu.page.ts
+++ src/app/pages/stock/reception/reception-menu/reception-menu.page.ts
@@ -1,13 +1,9 @@
 import {Component} from '@angular/core';
-import {CardListConfig} from '@common/components/card-list/card-list-config';
-import {Preparation} from '@entities/preparation';
-import {CardListColorEnum} from '@common/components/card-list/card-list-color.enum';
-import {NavService} from '@app/services/nav/nav.service';
-import {MainHeaderService} from '@app/services/main-header.service';
-import {SqliteService} from '@app/services/sqlite/sqlite.service';
-import {NavPathEnum} from '@app/services/nav/nav-path.enum';
-import * as moment from "moment";
 import {ViewWillEnter} from "@ionic/angular";
+import {CardListConfig} from "@common/components/card-list/card-list-config";
+import {CardListColorEnum} from "@common/components/card-list/card-list-color.enum";
+import * as moment from 'moment';
+import {ApiService} from "@app/services/api.service";
 
 @Component({
     selector: 'wii-reception-menu',
@@ -15,11 +11,74 @@ import {ViewWillEnter} from "@ionic/angular";
     styleUrls: ['./reception-menu.page.scss'],
 })
 export class ReceptionMenuPage implements ViewWillEnter {
-    public hasLoaded: boolean = false;
+    public hasLoaded: boolean;
 
-    public constructor() {
-    }
+    public receptionsListConfig: Array<CardListConfig>;
+    public readonly receptionsListColor = CardListColorEnum.PINK;
+    public readonly receptionsIconName = 'reception.svg';
+
+
+    public constructor(private apiService: ApiService) {}
 
     public ionViewWillEnter(): void {
+        this.hasLoaded = false;
+        // @ts-ignore
+        this.receptions =  this
+            .apiService.requestApi(ApiService.GET_RECEPTIONS, {})
+            .subscribe({
+                next: (response: { success: any; data: { expectedDate: string; }[]; }) => {
+                    if (response.success){
+                        console.log(response.data);
+                        this.receptionsListConfig = response.data
+                            .sort((a: { expectedDate: string; }, b: { expectedDate: string; }) => moment(a.expectedDate).diff(moment(b.expectedDate)))
+                            .map((reception: any ) => ({
+                                title: {
+                                    label: 'Réception',
+                                    value: reception.number
+                                },
+                                content: [
+                                    {
+                                        label: 'Statut',
+                                        value: reception.status
+                                    },
+                                    {
+                                        label: 'Fournisseur',
+                                        value: reception.supplier
+                                    },
+                                    {
+                                        label: 'Numéro de commande',
+                                        value: reception.orderNumber?.join(', ')
+                                    },
+                                    {
+                                        label: 'Date attendue',
+                                        value: moment(reception.expectedDate).format('DD/MM/YYYY')
+                                    }, {
+                                        label: 'Utilisateur',
+                                        value: reception.user
+                                    }, {
+                                        label: 'Transporteur',
+                                        value: reception.carrier
+                                    }, {
+                                        label: 'Emplacement',
+                                        value: reception.location
+                                    }
+                                ],
+                                ...(reception.emergency
+                                    ? {
+                                        rightIcon: {
+                                            name: 'exclamation-triangle.svg',
+                                            color: 'danger'
+                                        }
+                                    }
+                                    : {}),
+                                action: () => {
+                            }
+                        }));
+                    }
+                },
+                complete: () => {
+                    this.hasLoaded = true;
+                }
+            });
     }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -93,6 +93,7 @@ export class ApiService {
     public static readonly POST_RECEIPT_ASSOCIATION = {method: POST, service: '/receipt-association'};
     public static readonly CHECK_MANUAL_COLLECT_SCAN = {method: GET, service: '/check-manual-collect-scan'};
     public static readonly FINISH_MANUAL_COLLECT = {method: POST, service: '/finish-manual-collect'};
+    public static readonly GET_RECEPTIONS = {method: GET, service: '/mobile/reception/list'};
 
 
     private static readonly DEFAULT_HEADERS = {
